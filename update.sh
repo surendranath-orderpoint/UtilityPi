a3a02af125f5e4bd5f4f4eb0e590753394360ad9
Surendranath <38678415+surendranath-orderpoint@users.noreply.github.com>
2020-04-06 21:55:29 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,13 @@
 pwd
 #chmod 7 UtilityPi
 #cd UtilityPi
+cd ..
+mkdir UtilityPiNew
+cp UtilityPi/Utilitypi.zip UtilityPiNew/
+cp UtilityPi/node_modules/ UtilityPiNew/node_modules/
+cd UtilityPiNew
 unzip Utilitypi.zip
-\cp -rf UtilityPi/ .
+cp -rf UtilityPi/ .
 rm -rf Utilitypi.zip
 rm -rf UtilityPi
 pm2 restart oputility
