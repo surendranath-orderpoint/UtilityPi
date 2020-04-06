9035c43a5acf97bc814f1eea8958093725bd3d23
Surendranath <38678415+surendranath-orderpoint@users.noreply.github.com>
2020-04-06 22:09:13 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,11 +7,12 @@ cp UtilityPi/Utilitypi.zip UtilityPiNew/
 cp -R UtilityPi/node_modules/ UtilityPiNew/node_modules/
 cd UtilityPiNew
 unzip Utilitypi.zip
-cp -rf UtilityPi/ .
-echo "copy done"
+#cp -rf UtilityPi/ .
+#echo "copy done"
 rm -rf Utilitypi.zip
 rm -rf UtilityPi
 echo "Removed src files"
-pm2 stop oputility
-pm2 delete oputility
+rm -rf ../UtilityPi
+cd ..
+mv UtilityPiNew UtilityPi
 pm2 restart oputility
