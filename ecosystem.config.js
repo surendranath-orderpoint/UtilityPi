233742a27d89c2d1af331cef78579857c0c3bb68
Surendranath <gsurendranath@orderpoint.net.au>
2020-04-12 15:27:18 +0530

    updated ignore watch for vault

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
     script: 'index.js',
     autorestart: true,
     watch: true,
-    ignore_watch : ["node_modules", "temp"],
+    ignore_watch : ["node_modules", "vault"],
     watch_delay: 7000,
     max_memory_restart: '1G'
   }]
